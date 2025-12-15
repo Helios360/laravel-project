839f9d0933697ff4ddffd0be02e2d3a8d4dd2830
Helios360 <elias.treguier@gmail.com>
2025-12-15 11:55:50 +0100

    activité 4

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-        return "Bienvenue sur la page d'acceuil";
+        return view('welcome');
     }
 }
