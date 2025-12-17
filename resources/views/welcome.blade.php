12449010e8d0e8dc57606baf458be88291fbb231
Helios360 <elias.treguier@gmail.com>
2025-12-17 09:44:24 +0100

    Activité 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,11 @@
 @section('title', 'Accueil')
 @section('content')
     <h2> Bienvenue sur le site de {{ $name }}</h2>
-
-    <h2>La version loop</h2>
     <div style="display:flex; gap:1rem;">
         @foreach($articles as $article)
+            @if($loop->last)
+                @break
+            @endif
             <x-article :titre="$article['titre']" :desc="$article['contenu']"/>
         @endforeach
     </div>
